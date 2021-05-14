4939d2fa1ea33efecb6be3e1b0e12102e4571bff
Ira <irasoro402@yandex.ru>
2021-05-15 00:51:00 +0300

    Init commit

---
+++ mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QSqlDatabase db;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("C:/Users/User/Desktop/База_Данных/BD_Velosiped.db");
+    db.open();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString LineEditLogin = ui->lineEdit->text();
+    QString LineEditParol = ui->lineEdit_2->text();
+
+    QSqlQuery query;
+
+    if (query.exec("SELECT login_klienta, parol_klienta FROM Klient WHERE login_klienta = \'" +
+                   LineEditLogin + "\' AND parol_klienta = \'" + LineEditParol + "\'")){
+
+        if (query.next()){
+            login = query.value(0).toString();
+            parol = query.value(1).toString();
+            ui->textEdit->insertPlainText(login + " " + parol + "\n");
+
+        }
+        else {
+            ui->textEdit->insertPlainText("[-] Wrong Username or Password\n");
+        }
+    }
+
+}
